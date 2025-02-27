48ea4938219d86f15ddae59f43a2f191b862535a
GuilhermeFuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-02-27 19:23:45 -0300

    Add files via upload

--- loja.c
+++ loja.c
@@ -1,18 +1,21 @@
-/*vamo faze o menu principal eba
-usar do while pra ele ficar no programa ate pipipipopopo*/
 #include <stdio.h>
 
 int main(){
-    char d1;
-    char d2;
-    char de;
+    //variáveis de decisão
+    char d1; //decisão: abrir menu principal
+    char d2; //decisão: opções do menu principal
+    char d3; //decisão: opções dos submenus
+    
+    //abrir menu principal
     printf("Deseja abrir o menu? (S para Sim, N para Não): ");
     scanf(" %c", &d1);
     while(d1!='s' && d1!='n'){
         printf("Comando inválido, digite novamente: ");
         scanf(" %c", &d1);
     }
+    
     do{
+        //abrir submenus
         switch(d1){
             case 's':
                 printf("O que deseja?\n");
@@ -25,36 +28,62 @@ int main(){
                 printf("Comando inválido, digite novamente: ");
                 scanf(" %c", &d2);
                 }
+                
                 switch(d2){
+                    //submenu estoque
                     case 'e':
                         printf("Estoque\n");
-                        printf("Cadastrar novo item (digite C)\n");
-                        printf("Editar itens (digite E)\n");
-                        printf("Abrir estoque (digite A)\n");
+                        printf("Itens Disponíveis:\n 1. Item\n 2. Item\n 3. Item\n");
                         printf("Voltar (digite V)\n");
-                        scanf(" %c", &de);
-                        switch(de){
-                            case 'v':
-                            
+                        scanf(" %c", &d3);
+                        while(d3!='v'){
+                            printf("Comando inválido, digite novamente:  ");
+                            scanf(" %c", &d3);
                         }
                         break;
+                        
+                    //submenu vendas
                     case 'v':
-                        printf("Venda\n");
+                        printf("Vendas\n");
+                        printf("Itens Disponíveis:\n 1. Item\n 2. Item\n 3. Item\n");
+                        printf("Voltar (digite V)\n");
+                        scanf(" %c", &d3);
+                        while(d3!='v'){
+                            printf("Comando inválido, digite novamente:  ");
+                            scanf(" %c", &d3);
+                        }
                         break;
+                        
+                    //submenu financeiro
                     case 'f':
                         printf("Financeiro\n");
+                        printf("Registrar Despesas (digite D)\n");
+                        printf("Registrar Reveitas (digite R)\n");
+                        printf("Consultar Saldo (digite S)\n");
+                        printf("Voltar (digite V)\n");
+                        scanf(" %c", &d3);
+                        while(d3=='d' || d3=='r' || d3=='s'){
+                            printf("essa parte ainda não foi programada, então volta aí na moral\n");
+                            scanf(" %c", &d3);
+                        }
+                        while(d3!='d' && d3!='r' && d3!='s' && d3!='v'){
+                            printf("Comando inválido, digite novamente:  ");
+                            scanf(" %c", &d3);
+                        }
                         break;
+                        
+                    //encerrar programa
                     case 's':
                         printf("Encerrado");
-                        break;
+                        return 0;
                 }
-                
                 break;
-                
+            
+            //encerrar programa
             case 'n':
                 printf("Encerrado");
                 break;
         }
-    }while(de=='v' && d2=='s');
+    }while(d3=='v');
     return 0;
 }
