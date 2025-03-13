1178fe9ff20415037f7054bc23e0f5564fa4daef
GuilhermeFuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-03-13 09:36:27 -0300

    Add files via upload

--- comercio.c
+++ comercio.c
@@ -1,15 +1,32 @@
 //bibliotecas
 #include <stdio.h>
+#include <stdlib.h>
 
 //funções utilizadas
 int main();
-void submenus(char dec);
+int submenus(char dec, float sald);
 void estoque(char dec);
 void vendas(char dec);
-void financeiro(char dec);
+void financeiro(char dec, float sald);
+void despesas(char dec, float sald);
+void receitas(char dec, float sald);
+void saldo(char dec, float sald);
+
+int main() {
+	//variáveis de decisão
+	char d2; //decisão: opções do menu principal
+	
+	//variáaveis numéricas
+	float sald = 0; // saldo atual
+
+	//abrir submenus
+	submenus(d2, sald);
+	
+    return 0;
+}
 
 //abrir submenus
-void submenus(char dec) {
+int submenus(char dec, float sald) {
     //variáveis de decisão
 	char d3; //decisão: opções dos submenus
     
@@ -38,19 +55,20 @@ void submenus(char dec) {
 
 	//submenu financeiro
 	case 'f':
-		financeiro(d3);
+		financeiro(d3, sald);
 		break;
 
 	//encerrar programa
 	case 's':
 		printf("\nEncerrado");
+		exit(0);
 	}
 }
 
 //submenu estoque
 void estoque(char dec) {
 	printf("\nEstoque\n");
-	printf("Itens Disponíveis:\n 1. Item\n 2. Item\n 3. Item\n");
+	printf("Itens Disponíveis:\n 1. Bosta - 10 Unidades\n 2. Coco - 10 Unidades \n 3. Merda - 10 Unidades\n");
 	printf("Voltar (digite V)\n\n");
 	printf("Escolha: ");
 	scanf(" %c", &dec);
@@ -58,32 +76,42 @@ void estoque(char dec) {
 		printf("Comando inválido, digite novamente: ");
 		scanf(" %c", &dec);
 	}
-	if(dec=='v') {
-		main();
+	switch(dec){
+	    case 'v':
+	        main();
+	        break;
 	}
 }
 
 //submenu vendas
 void vendas(char dec) {
 	printf("\nVendas\n");
-	printf("Itens Disponíveis:\n 1. Item\n 2. Item\n 3. Item\n");
+	printf("Itens Disponíveis:\n 1. Bosta - R$15,00\n 2. Coco - R$20,00\n 3. Merda - R$45,00\n");
+	printf("Registrar Venda (digite R)\n");
 	printf("Voltar (digite V)\n\n");
 	printf("Escolha: ");
 	scanf(" %c", &dec);
-	while(dec!='v') {
+	while(dec!='v' && dec!='r') {
 		printf("Comando inválido, digite novamente: ");
 		scanf(" %c", &dec);
 	}
-	if(dec=='v') {
-		main();
+	switch(dec){
+	    
+	    case 'r':
+	        printf("Item Vendido:");
+	    
+	    case 'v':
+	        main();
+	        break;
 	}
 }
 
 //submenu financeiro
-void financeiro(char dec) {
+void financeiro(char dec, float sald) {
+    char d4; //decisão nas opções do submenu
 	printf("\nFinanceiro\n");
 	printf("Registrar Despesas (digite D)\n");
-	printf("Registrar Reveitas (digite R)\n");
+	printf("Registrar Receitas (digite R)\n");
 	printf("Consultar Saldo (digite S)\n");
 	printf("Voltar (digite V)\n\n");
 	printf("Escolha: ");
@@ -95,15 +123,15 @@ void financeiro(char dec) {
 	
 	switch(dec) {
         case 'd':
-            printf("vaisefode");
+            despesas(d4, sald);
 	        break;
 	    
 	    case 'r':
-	        printf("vaisefode");
+	        receitas(d4, sald);
 	        break;
 	        
 	    case 's':
-	        printf("vaisefode");
+	        saldo(d4, sald);
 	        break;
 	        
 	    case 'v':
@@ -113,13 +141,63 @@ void financeiro(char dec) {
 	}
 }
 
-int main() {
-	//variáveis de decisão
-	char d2; //decisão: opções do menu principal
-	char d3; //decisão: opções dos submenus
+//submenu financeiro: opção despesas
+void despesas(char dec, float sald) {
+    char d3; //decisão: opções dos submenus
+    float des; //valor da despesa
+    printf("\nValor da Despesa: R$");
+	scanf(" %f", &des);
+	sald = sald - des;
+	printf("Receita Registrada!\n");
+	financeiro(d3, sald);
+    scanf(" %c", &dec);
+    while(dec!='v') {
+		printf("Comando inválido, digite novamente: ");
+		scanf(" %c", &dec);
+	}
+	switch(dec) {
+        case 'v':
+            financeiro(d3, sald);
+            break;''
+    }
+}
 
-	//abrir submenus
-	submenus(d2);
-	
-    return 0;
+//submenu financeiro: opção receitas
+void receitas(char dec, float sald) {
+    char d3; //decisão: opções dos submenus
+    float rec; //valor da receita
+    printf("\nValor da Receita: R$");
+	scanf(" %f", &rec);
+	sald = sald + rec;
+	printf("Despesa Registrada!\n");
+	financeiro(d3, sald);
+    scanf(" %c", &dec);
+    while(dec!='v') {
+		printf("Comando inválido, digite novamente: ");
+		scanf(" %c", &dec);
+	}
+	switch(dec) {
+        case 'v':
+            financeiro(d3, sald);
+            break;
+    }
+}
+
+//submenu financeiro: opção saldo
+void saldo(char dec, float sald) {
+    char d3; //decisão: opções dos submenus
+    printf("\nSeu Saldo Atual\n");
+    printf("R$%.2f \n", sald);
+    printf("Voltar (digite V)\n\n");
+    printf("Escolha: ");
+    scanf(" %c", &dec);
+    while(dec!='v') {
+		printf("Comando inválido, digite novamente: ");
+		scanf(" %c", &dec);
+	}
+    switch(dec) {
+        case 'v':
+            financeiro(d3, sald);
+            break;
+    }
 }
