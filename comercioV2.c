96d9701e59e0a3ea8011b1f266b15059f083e2d0
GuilhermeFuzinatto <159266519+GuilhermeFuzinatto@users.noreply.github.com>
2025-04-09 08:28:25 -0300

    Add files via upload

--- comercioV2.c
+++ comercioV2.c
@@ -21,20 +21,36 @@ void despesas();
 void receitas();
 void saldo();
 
-void testador();
+//void testador();
 
 //variáveis globais
-#define tam 50 //tamanho geral dos vetores
+//#define tam 50 //tamanho geral dos vetores
 int i = 3; //espaço atual a ser preenchido nos vetores
 float sald = 0; //saldo total
+/*
 char idinput[tam]; //guarda inputs de id
 char nomeinput[tam]; //guarda inputs de nome
+*/
+
+struct Item{
+    float vend;
+    int qtdvend;
+    int uni;
+    float preco;
+    char id[3];
+    char nome[50];
+};
+
+struct Item itens[50];
+
+/*
 float vend[tam] = {0, 0, 0}; //valor vendido por cada item
 int qtdvend[tam] = {0, 0, 0}; //quantidade vendida de cada item
 int uni[tam] = {10, 10, 10}; //quantidade dos itens em estoque
 float preco[tam] = {15, 20, 45}; //preço dos itens em estoque
 char*id[tam] = {"001", "002", "003"}; //id dos itens em estoque
 char*nome[tam] = {"Bosta", "Coco", "Merda"}; //nome dos itens em estoque
+*/
 int d; //decisões no programa
 
 int main() {
@@ -88,7 +104,7 @@ int submenus() {
 	//encerrar programa
 	case 0:
 	    for(int cont = 0; cont < i; cont++){
-	        printf("\nVendas Item %s: R$%.2f ; Quantidade: %d", id[cont], vend[cont], qtdvend[cont]);
+	        printf("\nVendas Item %s: R$%.2f ; Quantidade: %d", itens[cont].id, itens[cont].vend, itens[cont].qtdvend);
     	}
 	    printf("\nSaldo final: R$%.2f", sald);
 		printf("\n\nEncerrado");
@@ -103,7 +119,7 @@ void estoque() {
 	//visualizaçãao dos itens
 	printf("Itens Disponíveis:\n");
 	for(int cont = 0; cont < i; cont++){
-	    printf("%s. %s - %d Unidades\n", id[cont], nome[cont], uni[cont]);
+	    printf("%s. %s - %d Unidades\n", itens[cont].id, itens[cont].nome, itens[cont].uni);
 	}
 	
 	printf("Adicionar Item (digite 1)\n");
@@ -135,21 +151,18 @@ void additem(){
     
     //atribuir ID automaticamente ao item
     if(i<10){
-        sprintf(idinput, "00%d", (i + 1));
-        id[i] = idinput;
+        sprintf(itens[i].id, "00%d", (i + 1));
     }else if(i<100){
-        sprintf(idinput, "0%d", (i + 1));
-        id[i] = idinput;
+        sprintf(itens[i].id, "0%d", (i + 1));
     }else{
-        sprintf(idinput, "%d", (i + 1));
-        id[i] = idinput;
+        sprintf(itens[i].id, "%d", (i + 1));
     }
     
     //atribuir nome ao item
     printf("Nome do Item: ");
-    fgets(nomeinput, sizeof(nomeinput), stdin);
-    fgets(nomeinput, sizeof(nomeinput), stdin);
-    strtok(nomeinput, "\n");
+    fgets(itens[i].nome, sizeof(itens[i].nome), stdin);
+    fgets(itens[i].nome, sizeof(itens[i].nome), stdin);
+    strtok(itens[i].nome, "\n");
     
     //ARRUMAR TRATAMENTO DE ERRO
     /*
@@ -162,15 +175,13 @@ void additem(){
     }
     */
     
-    nome[i] = nomeinput;
-    
     //atribuir unidades ao item
     printf("Unidades Disponíveis: ");
-    scanf(" %d", &uni[i]);
+    scanf(" %d", &itens[i].uni);
     
     //atribuir preço ao item
     printf("Preço do Item: ");
-    scanf(" %f", &preco[i]);
+    scanf(" %f", &itens[i].preco);
     
     i++;
     
@@ -189,7 +200,7 @@ void vendas() {
 	//visualizaçãao dos itens
 	printf("Itens Disponíveis:\n");
 	for(int cont = 0; cont < i; cont++){
-	    printf("%s. %s - R$%.2f\n", id[cont], nome[cont], preco[cont]);
+	    printf("%s. %s - R$%.2f\n", itens[cont].id, itens[cont].nome, itens[cont].preco);
 	}
 	printf("Registrar Venda (digite 1)\n");
 	printf("Total Vendido Por Item (digite 2)\n");
@@ -229,7 +240,7 @@ void vendas() {
 	        
 	        printf("Quantidade Vendida: ");
 	        scanf("%d", &qtd);
-	        while((item==001 && qtd>uni[0]) || (item==002 && qtd>uni[1]) || (item==003 && qtd>uni[2])){
+	        while((item==001 && qtd>itens[0].uni) || (item==002 && qtd>itens[1].uni) || (item==003 && qtd>itens[2].uni)){
 	            printf("Quantidade maior do que itens em estoque, digite novamente: ");
 	            scanf("%d", &qtd);
 	        }
@@ -252,9 +263,9 @@ void vendas() {
 	                printf("\n\nTotal vendido: R$%.2f \n", total);
 	                printf("Receita Registrada! \n");
 	                sald = sald + total;
-	                vend[0] = vend[0] + total;
-	                qtdvend[0] = qtdvend[0] + qtd;
-	                uni[0] = uni[0] - qtd;
+	                itens[0].vend = itens[0].vend + total;
+	                itens[0].qtdvend = itens[0].qtdvend + qtd;
+	                itens[0].uni = itens[0].uni - qtd;
 	                vendas();
 	                break;
 	               
@@ -264,9 +275,9 @@ void vendas() {
 	                printf("\n\nTotal vendido: R$%.2f \n", total);
 	                printf("Receita Registrada! \n");
 	                sald = sald + total;
-	                vend[1] = vend[1] + total;
-	                qtdvend[1] = qtdvend[1] + qtd;
-	                uni[1] = uni[1] - qtd;
+	                itens[1].vend = itens[1].vend + total;
+	                itens[1].qtdvend = itens[1].qtdvend + qtd;
+	                itens[1].uni = itens[1].uni - qtd;
 	                vendas();
 	                break;
 	                
@@ -276,9 +287,9 @@ void vendas() {
 	                printf("\n\nTotal vendido: R$%.2f \n", total);
 	                printf("Receita Registrada! \n");
 	                sald = sald + total;
-	                vend[2] = vend[2] + total;
-	                qtdvend[2] = qtdvend[2] + qtd;
-	                uni[2] = uni[2] - qtd;
+	                itens[2].vend = itens[2].vend + total;
+	                itens[2].qtdvend = itens[2].qtdvend + qtd;
+	                itens[2].uni = itens[2].uni - qtd;
 	                vendas();
 	                break;
 	        }
@@ -347,7 +358,7 @@ float pagamento(float total){
 void totalpitem(float total, int item){
     printf("Valor e Quantidade Vendida por Item");
 	for(int cont = 0; cont < i; cont++){
-	    printf("\nItem %s - Valor Vendido: R$%.2f ; Quantidade: %d", id[cont], vend[cont], qtdvend[cont]);
+	    printf("\nItem %s - Valor Vendido: R$%.2f ; Quantidade: %d", itens[cont].id, itens[cont].vend, itens[cont].qtdvend);
 	}
 	printf("\nVoltar (digite 0)\n");
 	printf("Escolha: ");
@@ -463,6 +474,7 @@ void saldo() {
     }
 }
 
+/*
 void testador() {
     printf("\nBEM VINDO AO TESTADOR ONDE TUDO É TESTADO\n");
     printf("\nID's\n");
@@ -500,3 +512,4 @@ void testador() {
 	        break;
 	}
 }
+*/
